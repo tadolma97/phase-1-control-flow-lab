613b9be3ee7df3370d87fcfaa2960c9b37166f86
Tashi Dolma <tadolma97@gmail.com>
2022-04-25 08:00:24 -0400

    commit tree c3750d44fd8e9b80de2ebb245ecde3057e5e5427

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  if (number<= 400){
+    return 'This one is on me!';
+  }
+  else if (number>400 && number<2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (number>2000&&number<2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (number>2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city==='NYC' ? "Ok, sounds good.":"No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+  }
 }
